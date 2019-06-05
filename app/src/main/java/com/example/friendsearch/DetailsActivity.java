438667e79f1aa4caf2e2e820799c1e591cc974ad
jo <joannasarasusan@gmail.com>
2019-06-05 20:13:29 +0700

    Linking the buttons.

--- app/src/main/java/com/example/friendsearch/DetailsActivity.java
+++ app/src/main/java/com/example/friendsearch/DetailsActivity.java
@@ -1,13 +1,51 @@
 package com.example.friendsearch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+import static com.example.friendsearch.R.array.friend_details;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    HashMap<String, Integer> peopleIndex;
+    int[] photos = {
+            R.drawable.boy1,
+            R.drawable.boy2,
+            R.drawable.boy3,
+            R.drawable.girl1,
+            R.drawable.boy4,
+            R.drawable.boy5
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        peopleIndex = new HashMap<>();
+        peopleIndex.put("Chandler", 0);
+        peopleIndex.put("Joey", 1);
+        peopleIndex.put("Monica", 2);
+        peopleIndex.put("Phoebe", 3);
+        peopleIndex.put("Rachel", 4);
+        peopleIndex.put("Ross", 5);
+
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("people_name");
+
+        int positionInArray = peopleIndex.get(name);
+        ImageView imageView = findViewById(R.id.details_imageview);
+        imageView.setImageResource(photos[positionInArray]);
+
+        TextView textView = (TextView) findViewById(R.id.details_textview);
+        String[] friendDetails = getResources().getStringArray(friend_details);
+        textView.setText(friendDetails[positionInArray]);
+
     }
 }
